d2f6e38f46a1d9ed3fd27f676a27311b4f85421c
nikhilkuria <nikhilkuria@gmail.com>
2018-09-23 21:30:17 +0200

    Adding an additional param to player yoy

--- src/commands/player.py
+++ src/commands/player.py
@@ -1,4 +1,5 @@
 import logging
+from typing import List
 
 from nubia import command, argument
 
@@ -13,8 +14,20 @@ class Player:
     """Use this to see stats and performance of your favourite player"""
 
     @command
-    @argument("player_name", positional=True)
-    def year_on_year(self, player_name: str):
+    @argument("player_name", description="name of the player ", positional=True)
+    @argument("stat_names", description="stats", choices=[
+        "GAMES PLAYED",
+        "GAMES STARTED",
+        "MINUTES",
+        "AVG POINTS",
+        "AVG ASSISTS",
+        "AVG REBOUNDS",
+        "AVG STEALS",
+        "AVG BLOCKS",
+        "AVG TURNOVERS",
+        "AVG PERSONAL FOULS"
+    ])
+    def year_on_year(self, player_name: str, stat_names: List[str] = None):
         """
         print the player's year on year statistics
         """
@@ -27,8 +40,13 @@ class Player:
 
             player_stats_response = player_stats.get_player_stats(first_name, last_name)
             player_yoy = player_stats.get_progress_from_player_stats(player_stats_response)
-            for stat_name, stats in player_yoy.items():
-                print(stacked_graph.build_stacked_graph(list(stats.keys()), list(stats.values()), stat_name))
+            if stat_names:
+                for stat_name in stat_names:
+                    stats = player_yoy[stat_name]
+                    print(stacked_graph.build_stacked_graph(list(stats.keys()), list(stats.values()), stat_name))
+            else:
+                for stat_name, stats in player_yoy.items():
+                    print(stacked_graph.build_stacked_graph(list(stats.keys()), list(stats.values()), stat_name))
         except PlayerNotFoundException:
             error_message = 'No player found for the given name, {first_name} {last_name}'\
                             .format(first_name=first_name,
